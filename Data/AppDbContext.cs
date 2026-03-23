e239a094632ab64b768efbf32baaf4565950febd
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-23 08:50:33 +0500

    added db in appdbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AuthPracticeAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthPracticeAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+           public AppDbContext(DbContextOptions<AppDbContext>options)
+            : base(options)
+        {
+
+        }
+    }
+}
